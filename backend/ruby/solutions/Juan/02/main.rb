c649c08433a44613e07f0949701567aa1973822a
Juan Moreno <jose.morenobrn@uanl.edu.mx>
2024-12-11 11:26:17 -0600

    refactor: change camelcase to snakecase

--- backend/ruby/solutions/Juan/02/main.rb
+++ backend/ruby/solutions/Juan/02/main.rb
@@ -2,12 +2,11 @@ REVIEWERS = ['Javier', 'David', 'Sebastián']
 REVIEWEES = ['Brandon', 'Amanda', 'Tovar']
 
 def main
-
-    for reviewee in REVIEWEES
-        reviewersSelected = REVIEWERS.sample(2)
+    REVIEWEES.each do |reviewee|
+        reviewersSelected = REVIEWERS.sample 2
         sendTo = "E-mail to: #{reviewee.downcase}@localsolutionit.com\n"
         greeting = "Body:\n\nHi, #{reviewee}.\n"
-        body = "Your reviewers are #{reviewersSelected[0]} and #{reviewersSelected[0]}"
+        body = "Your reviewers are #{reviewersSelected[0]} and #{reviewersSelected[1]}"
         message =  "#{sendTo}#{greeting}#{body}."
         puts message
         File.open("#{reviewee.downcase}@localsolutionit.txt", "w") do |f|     
@@ -16,4 +15,4 @@ def main
     end
 end
 
-main()
+main
