fdffa46cdfb13dab5cdda00779bff8b5fd65c9e8
Juan Moreno <jose.morenobrn@uanl.edu.mx>
2024-12-11 10:44:49 -0600

    refactor: Improve the syntax in the code

--- backend/ruby/solutions/Juan/01/main.rb
+++ backend/ruby/solutions/Juan/01/main.rb
@@ -1,14 +1,14 @@
-$developers = [];
+DEVELOPERS = []
 
 def addUser(user)
-    user["id"] = $developers.length + 1
-    $developers.push(user)
-    skillsMsg = ""
+    user["id"] = DEVELOPERS.length + 1
+    DEVELOPERS.push user
+    skills_msg = ""
 
-    for skill in user[:skills]
-        skillsMsg = "#{skill}, #{skillsMsg}"
+    user[:skills].each do |skill|
+        skills_msg = "#{skill}, #{skills_msg}"
     end  
-    message = "#{user[:name]} is a #{user[:age]} old developer with #{user[:friends]} friends and is able to #{skillsMsg}"
+    message = "#{user[:name]} is a #{user[:age]} old developer with #{user[:friends]} friends and is able to #{skills_msg}"
     puts "#{message.strip.chop}." 
 
 end
@@ -20,7 +20,7 @@ def validateInteger(message)
         puts message
         return Integer(gets)
     rescue ArgumentError => e
-        puts "Solo se permiten numeros enteros"
+        puts "Solo se permiten numeros enteros #{e}"
     retry
     end
 
@@ -28,34 +28,25 @@ end
 
 def getInfoUser()
     puts "Ingresa tu nombre"
-    name = gets.chomp.to_s;
+    name = gets.chomp.to_s
     age = validateInteger("Ingresa tu edad")
     friends = validateInteger("Ingresa tu número de amigos")
 
     { name: name, age: age, friends: friends, skills: addSkills }
 end
 
-
 def addSkills()
     skills = []
-    addSkill = true
+    addSkill = '1'
     system("clear")
     
-    while addSkill == true
+    while addSkill == '1'
         puts "Añade una habilidad"
         skill = gets.chomp.to_s
-        skills.push(skill)
+        skills.push skill
         puts "Añadir nueva habilidad?"
-        puts "0 No; 1 Si"
-        
-        add = gets.chomp.to_i
-
-        if add == 1
-            addSkill = true
-
-        else 
-            addSkill = false
-        end
+        puts "0 No 1 Si"
+        addSkill = gets.chomp.to_s
         system("clear")
     end
 
@@ -64,24 +55,16 @@ def addSkills()
 end
 
 def main 
-    addNewUser = true
-    while addNewUser == true
+    addNewUser = '1'
+    while addNewUser == '1'
         user = getInfoUser
-        addUser(user)
+        addUser user
 
         puts "Añadir un nuevo usuario?"
-        puts "0 No; 1 Si"
-        
-        add = gets.chomp.to_i
-
-        if add == 1
-            addNewUser = true
-
-        else 
-            addNewUser = false
-
-        end
+        puts "0 No 1 Si"
+             
+        addNewUser = gets.chomp.to_s
     end
 end
 
-main()
+main
