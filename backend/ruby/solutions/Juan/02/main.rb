9915497471c1a912368ef2b9fcf916bd1bda3131
Juan Moreno <jose.morenobrn@uanl.edu.mx>
2024-12-11 12:02:09 -0600

    refactor: improve syntax

--- backend/ruby/solutions/Juan/02/main.rb
+++ backend/ruby/solutions/Juan/02/main.rb
@@ -3,13 +3,13 @@ REVIEWEES = ['Brandon', 'Amanda', 'Tovar']
 
 def main
     REVIEWEES.each do |reviewee|
-        reviewersSelected = REVIEWERS.sample 2
+        reviewers_selected = REVIEWERS.sample 2
         sendTo = "E-mail to: #{reviewee.downcase}@localsolutionit.com\n"
         greeting = "Body:\n\nHi, #{reviewee}.\n"
-        body = "Your reviewers are #{reviewersSelected[0]} and #{reviewersSelected[1]}"
+        body = "Your reviewers are #{reviewers_selected[0]} and #{reviewers_selected[1]}"
         message =  "#{sendTo}#{greeting}#{body}."
         puts message
-        File.open("#{reviewee.downcase}@localsolutionit.txt", "w") do |f|     
+        File.open "#{reviewee.downcase}@localsolutionit.txt", "w" do |f|     
             f.write(message)   
         end   
     end
