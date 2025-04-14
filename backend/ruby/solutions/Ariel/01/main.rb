1d243e29efb6de8aa4ae0c9e45a231d6608ae143
Ariel1009 <ariel.campavo@uanl.edu.mx>
2025-04-14 12:59:57 -0600

    style: fix the style of code

--- backend/ruby/solutions/Ariel/01/main.rb
+++ backend/ruby/solutions/Ariel/01/main.rb
@@ -1,10 +1,10 @@
-evelopers = []
+developers = []
 
 def add(operator)
-    puts "What's your name?: "
+    puts 'Whats your name?: '
     name = gets.chomp
 
-    puts "How old are you?: "
+    puts 'How old are you?: '
     age = gets.chomp.to_i
 
     skills = []
@@ -13,85 +13,89 @@ def add(operator)
     id = operator.length + 1
     person = {id: id, name: name, age: age, skills: skills, friends: friend}
 
-    operator << person
+    operator.push person
 
-    pausa
+    pause
 end
 
 def show (operador)
-    return puts "there's no developers" if operador.empty?
-    puts "Developers"
+    return puts 'theres no developers' if operador.empty?
+    puts 'Developers'
     operador.each do |devs| 
-        puts "Name: " + "#{devs[:name].to_s}"
+        puts "Name: #{devs[:name].to_s}"
     end
 end
 
-def add_Skill (operator)
-    show(operator)
-    puts "What's the name you search?: "
+def add_skill operator
+    show operator
+    puts 'Whats the name you search?: '
     name = gets.chomp
 
     operator.each do |dev|
         if dev[:name] == name
-          puts "What's your new skill?: "
+          puts 'Whats your new skill?: '
           skill = gets.chomp
-          dev[:skills] << skill 
+          dev[:skills].push skill
         end
     end
-    pausa
+
+    pause
 end
 
-def add_Friend(operator)
-    show(operator)
-    puts "What's the name you search?: "
+def add_friend operator
+    show operator
+    puts 'Whats the name you search?: '
     name = gets.chomp
 
     operator.each do |dev|
         if dev[:name] == name
-          puts "Write the name of your new friend: "
+          puts 'Write the name of your new friend: '
           friend = gets.chomp
-          dev[:friends] << friend    
+          dev[:friends].push friend    
         end
     end
     
-    pausa
+    pause
 end
 
-def pausa ()
-    puts "Presiona Enter para continuar..." 
+def pause
+    puts 'Presiona Enter para continuar...' 
     gets
-    system('clear')
+    system 'clear'
 end
 
-def show_Devs(operator)
+def show_devs operator
     puts 'There are not Devs' if operator.empty?
-  
+
     operator.each do |dev|
-    
       count_friends = dev[:friends].length
-  
       puts "#{dev[:name]} is a #{dev[:age]} years old with #{count_friends} friend and is able to #{dev[:skills] * ", "}" + ".\n"
-      
     end
-    pausa
+
+    pause
 end
 
-def menu(developers)
-    puts "\nMenu.\n1.- Add Dev.\n2.- Add Skills.\n3.- Add Friend.\n4.- Show Devs.\n5.- Exit."
+def menu  developers
+    puts 'Menu.
+    1.- Add Dev.
+    2.- Add Skills.
+    3.- Add Friend.
+    4.- Show Devs.
+    5.- Exit.'
     
+    puts 'Choose an option: ' 
     option = gets.chomp.to_i
     
-    case option
-    when 1
-      add(developers)
-    when 2
-      add_Skill(developers)
-    when 3
-      add_Friend(developers)
-    when 4
-      show_Devs(developers)
-    when 5
-        puts "Presione Enter para salir..." 
+    if option == 1
+      add developers
+    elsif option == 2
+      add_skill developers
+    elsif option == 3
+      add_friend developers
+    elsif option == 4
+      show_devs developers
+    elsif option == 5
+        puts 'Presione Enter para salir...' 
         gets
         false
     else 
